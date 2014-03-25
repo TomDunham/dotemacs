d8378d0dcdc45eef6bdb45fe9b46a56d000afddf
Thomas Dunham <tom@secondsync.com>
2014-03-25 21:02:49 +0000

    UTF-8 encoding

--- init.el
+++ init.el
@@ -93,6 +93,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(safe-local-variable-values (quote ((encoding . utf-8))))
  '(uniquify-buffer-name-style (quote post-forward) nil (uniquify))
  '(whitespace-style (quote (face empty tabs lines-tail trailing))))
 
