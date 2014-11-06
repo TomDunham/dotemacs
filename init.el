a53e0039d05b937999f2ab68da2b98a3cee96e91
Tom Dunham <tdunham@twitter.com>
2014-11-06 17:45:30 +0000

    indent tabs mode

--- init.el
+++ init.el
@@ -98,6 +98,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(indent-tabs-mode nil)
  '(safe-local-variable-values (quote ((encoding . utf-8))))
  '(uniquify-buffer-name-style (quote post-forward) nil (uniquify))
  '(whitespace-style (quote (face empty tabs lines-tail trailing))))
