bbab0cecfd0d0bbad6d69c7672f5a8f591c23ee1
Tom Dunham <tdunham@slack-corp.com>
2017-09-04 09:35:43 +1000

    Bunch of mode stuff

--- init.el
+++ init.el
@@ -41,32 +41,37 @@
 (global-whitespace-mode t)  ;; configured via customize
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
 
-
-;; Faster than default tramp scp, according to emacswiki
-(setq tramp-default-method "ssh")
-
-
 ;; Package manager
 (when (>= emacs-major-version 24)
   (require 'package)
   (package-initialize)
   (add-to-list
-   'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t))
+   'package-archives '("melpa" . "https://melpa.org/packages/") t))
+
+;; local lisp
+(add-to-list 'load-path "~/.emacs.d/lisp/")
+
+(use-package ack
+  :ensure t)
 
 (use-package browse-kill-ring
   :ensure t)
 
+(use-package cider :ensure t)
 
-(use-package magit
-  :init (global-set-key "\M-?" 'magit-status)
-  :ensure t)
-
-
-(use-package markdown-mode :ensure t)
-
-(use-package php-mode
+(use-package exec-path-from-shell
   :ensure t
-  :mode ("\\.php\\'" . php-mode))
+  :config (exec-path-from-shell-initialize))
+
+(use-package ggtags
+  :ensure t
+  :bind (("C-c g s" . ggtags-find-other-symbol)
+         ("C-c g h" . ggtags-view-tag-history)
+         ("C-c g r" . ggtags-find-reference)
+         ("C-c g f" . ggtags-find-file)
+         ("C-c g c" . ggtags-create-tags)
+         ("C-c g u" . ggtags-update-tags)
+         ("M-," . pop-tag-mark)))
 
 
 (use-package gitconfig-mode
@@ -77,6 +82,28 @@
   :ensure t
   :defer t)
 
+(use-package gotest
+  :ensure t)
+
+(use-package go-mode
+  :config
+  (defun my-go-mode-hook ()
+    ;; https://github.com/cockroachdb/cockroach/wiki/Ben's-Go-Emacs-setup
+    (local-set-key (kbd "M-.") #'godef-jump)
+    (add-hook 'before-save-hook 'gofmt-before-save)
+    (let ((map go-mode-map))
+        (define-key map (kbd "C-c a") 'go-test-current-project) ;; current package, really
+        (define-key map (kbd "C-c m") 'go-test-current-file)
+        (define-key map (kbd "C-c .") 'go-test-current-test)
+        (define-key map (kbd "C-c b") 'go-run)))
+  (add-hook 'go-mode-hook 'my-go-mode-hook)
+  :ensure t)
+
+
+(use-package flymake-go
+  :ensure t)
+
+
 (use-package guide-key
   :ensure t
   :diminish guide-key-mode
@@ -85,15 +112,52 @@
     (setq guide-key/popup-window-position 'bottom
           guide-key/guide-key-sequence t  ; enable for all prefixes
           guide-key/recursive-key-sequence-flag t)
-
     (guide-key-mode 1)))
 
+(use-package js2-mode
+  :ensure t)
+
+
+(use-package magit
+  :init (global-set-key "\M-?" 'magit-status)
+  :ensure t)
+
+(use-package markdown-mode :ensure t)
+
+(use-package mtail-mode)
+
+(use-package php-mode
+  :ensure t
+  :mode ("\\.php\\'" . php-mode)
+  :config
+  (defun slack-php-mode-hook ()
+    (setq tab-width 8)
+    (setq indent-tabs-mode t)
+    (setq c-basic-offset 8)
+    ;; slack-style requires the initial indent
+    ;; https://stackoverflow.com/a/1136007
+    (c-set-offset 'topmost-intro 8)
+    (c-set-offset 'cpp-macro -8))
+  (add-hook 'php-mode-hook 'slack-php-mode-hook))
+
+(use-package terraform-mode
+  :ensure t)
+
+(use-package vagrant-tramp
+  :ensure t)
 
 (use-package yaml-mode
   :ensure t
   :mode ("\\.yaml\\'" . yaml-mode))
 
 
+(use-package yapfify
+  :ensure t)
+
+(use-package editorconfig
+  :ensure t
+  :config
+  (editorconfig-mode 1))
 
 ;; if I want col markers again, prob fill-column-indicator
 ;; like this: https://github.com/bdd/.emacs.d/blob/master/packages.el#L76
@@ -115,8 +179,9 @@
  '(indent-tabs-mode nil)
  '(package-selected-packages
    (quote
-    (browse-kill-ring yaml-mode guide-key gitignore-mode gitconfig-mode php-mode use-package markdown-mode magit)))
+    (gotest editorconfig terraform-mode logstash-conf-mode ack flymake-go go-flymake js2-mode ggtags cider go-mode yapfify exec-path-from-shell vagrant-tramp browse-kill-ring yaml-mode guide-key gitignore-mode gitconfig-mode php-mode use-package markdown-mode magit)))
  '(safe-local-variable-values (quote ((encoding . utf-8))))
+ '(tramp-default-method "ssh")
  '(uniquify-buffer-name-style (quote post-forward) nil (uniquify))
  '(whitespace-style (quote (face empty tabs lines-tail trailing))))
 
@@ -129,4 +194,51 @@
  )
 
 (server-start)
+
+
+;; should be in own file
+;; find source code locations in browser
+
+;; Github enterprise URLs
+(setq s5-proj-map
+      '(("$HOME/proj/chef-repo" . "https://slack-github.com/slack/chef-repo/blob/%s/%s")
+        ("$HOME/proj/webapp" . "https://slack-github.com/slack/webapp/blob/%s/%s")
+        ("$HOME/proj/gosrc/src/slack-github.com/slack/ops-go.git" . "https://slack-github.com/slack/ops-go/blob/%s/%s")))
+
+
+(defun s5-proj-prefix-p (fn ele)
+    (string-prefix-p (substitute-in-file-name (car ele)) fn))
+
+(defun s5-line-no ()
+  (let ((start (point-min))
+	(n (line-number-at-pos)))
+    (if (= start 1)
+        n
+      (save-excursion
+	(save-restriction
+	  (widen)
+          (+ n (line-number-at-pos start) -1))))))
+
+
+(defun s5-ghe-url (fn br &optional ln)
+  ; url to view source file `fn`, branch `br`, line number `ln`
+  (concat
+   (format (cdr (seq-find (apply-partially 'proj-prefix-p fn)
+                          s5-proj-map))
+           br
+           (file-relative-name fn
+                               (substitute-in-file-name
+                                (car (seq-find (apply-partially 'proj-prefix-p fn)
+                                               s5-proj-map)))))
+   (if ln (format "#L%s" ln) "")))
+
+(defun s5-browse-ghe-url-at-line ()
+  (interactive)
+  (browse-url (s5-ghe-url
+               (buffer-file-name)
+               (car (vc-git-branches))
+               (s5-line-no))))
+
+
+
 ;;; init.el ends here
